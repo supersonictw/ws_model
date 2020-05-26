1f09e74ad4175a54a2dc1f3368b8c8c4c98e8b3f
SuperSonic <supersonic@livemail.tw>
2020-05-26 07:46:51 +0000

    Add client.py and server.py

---
+++ client.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+import sys
+import asyncio
+
+from tornado.ioloop import IOLoop, PeriodicCallback
+from tornado import gen
+from tornado.websocket import websocket_connect
+
+callback_url = "ws://localhost:8085/ws"
+
+class Client:
+    def __init__(self):
+        self.ws = None
+
+    async def connect(self):
+        self.ws = await websocket_connect(callback_url)
+        await self.run()
+
+    async def run(self):
+        while True:
+            msg = await self.ws.read_message()
+            if msg is None: break
+            self.ws.write_message("Hi Hi")
+            print(msg)
+
+if __name__ == "__main__":
+    client = Client()
+    loop = asyncio.get_event_loop()
+    asyncio.run(client.connect())
+    loop.run_forever()
---
+++ server.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+import json
+from abc import ABC
+
+from tornado.httpserver import HTTPServer
+from tornado.ioloop import IOLoop
+from tornado.web import Application, RequestHandler
+from tornado.websocket import WebSocketHandler
+
+class WSHandler(WebSocketHandler, ABC):
+    def check_origin(self, origin):
+        return True
+
+    def open(self):
+        result = {"status": 201}
+        self.write_message(json.dumps(result))
+
+    def on_message(self, message):
+        result = {"status": 202}
+        self.write_message(json.dumps(result))
+        for i in range(15):
+            self.write_message(str(i))
+
+    def on_close(self):
+        pass
+
+if __name__ == "__main__":
+    port = 8085
+    thread = 4
+
+    app = Application([
+        ('/ws', WSHandler)
+    ])
+    server = HTTPServer(app)
+    server.listen(port)
+    server.start(thread)
+    IOLoop.current().start()
